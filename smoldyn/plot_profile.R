662869d028bd19e54d1338a7cf54be239abeda3d
maxconway <mjc233@cam.ac.uk>
2013-02-27 16:52:53 +0000

    end of day wednesay: all simulates nicely, but many of the graphs are flat

--- smoldyn/plot_profile.R
+++ smoldyn/plot_profile.R
@@ -6,6 +6,7 @@ colnames(pbcd_profile) = c('time',seq(from=50,to=500,by=50))
 m = melt(pbcd_profile,id.vars="time")
 res = dcast(data=m,formula = time + variable ~ .)
 colnames(res) = c('time','distance','concentration')
-with(subset(res,time==8460),plot(distance,concentration),'.')
-with(res,scatterplot3d(distance,time,concentration,pch='o'))
+with(subset(res,time==8460),plot(distance,concentration),'.',main='at end')
+with(res,scatterplot3d(distance,time,concentration,pch='o',main='3d'))
+
 }
