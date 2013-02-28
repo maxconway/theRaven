9f83d899b35a60b44ba72f087fc0e17f5f40e898
maxconway <mjc233@cam.ac.uk>
2013-02-28 17:53:23 +0000

    end of thursday

--- smoldyn/generate_params.py
+++ smoldyn/generate_params.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
+import sys
 import csv
 
-with open('experiments.csv', 'rb') as csvfile:
+##args:
+#filename
+
+
+with open('./'+sys.argv[1], 'rb') as csvfile:
 	reader = csv.DictReader(csvfile,delimiter='	')
 	for row in reader:
 		with open('./experiments/'+row['name']+'.params','w') as output:
--- smoldyn/use_params.py
+++ smoldyn/use_params.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 import sys
+import glob
 
 ##args:
 #name
-#modelname
 
 with open('../fly-template', 'rb') as templatefile:
 	template = templatefile.read()
-	with open('./'+sys.argv[1], 'rb') as paramsfile:
-		params = paramsfile.read()
-		with open('./'+sys.argv[2],'wb') as output:
-			result = template.replace('RePlAcEmE',params,1)
-			output.write(result)
+	for filename in glob.glob('./*.params'):
+		with open(filename, 'rb') as paramsfile:
+			params = paramsfile.read()
+			with open(str.replace(filename,'.params','.smol'),'wb') as output:
+				result = template.replace('RePlAcEmE',params,1)
+				output.write(result)
 			
 			
 
