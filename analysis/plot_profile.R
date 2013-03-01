060e3dbb5ee195a164a3bae314effcffacfe44ee
maxconway <mjc233@cam.ac.uk>
2013-03-01 16:21:06 +0000

    all working awesomely!

--- analysis/plot_profile.R
+++ analysis/plot_profile.R
@@ -1,7 +1,7 @@
-plot_profile <- function(file){
+plot_profile <- function(file,mol){
 require('scatterplot3d')
 source('resshape_data.R')
 res = resshape_data(file)
 #with(subset(res,time==8460),plot(distance,concentration),'.',main='at end')
-with(res,scatterplot3d(distance,time,concentration,pch='o',main=file))
+with(res[res$molecule==mol,],scatterplot3d(distance,time,concentration,pch='o',main=paste(file,' --- ',mol))
 }
--- analysis/resshape_data.R
+++ analysis/resshape_data.R
@@ -1,9 +1,9 @@
 resshape_data <- function(file){
   require(reshape2)
   pbcd_profile = read.table(file,header=FALSE)
-  colnames(pbcd_profile) = c('time',seq(from=0.5,to=500,by=0.5))
-  m = melt(pbcd_profile,id.vars="time")
-  res = dcast(data=m,formula = time + variable ~ .)
-  colnames(res) = c('time','distance','concentration')
+  colnames(pbcd_profile) = c('molecule','time',seq(from=2.5,to=500,by=2.5))
+  m = melt(pbcd_profile,id.vars=c("time",'molecule'))
+  res = dcast(data=m,formula = molecule + time + variable ~ .)
+  colnames(res) = c('molecule','time','distance','concentration')
   return(res)
 }
