89e2961b4be094eeb0173401db109910ca08ed9f
maxconway <conway.max1@gmail.com>
2013-03-03 15:53:24 +0000

    now plotting bicoid graphs pleasantly

---
+++ analysis/bicoid_concs.R
@@ -0,0 +1,51 @@
+#needs:
+#flyex
+#3 rates
+#increasing rate during development
+#directed gradient at: fertilization, cycle 3
+flyex = read.table(file='bicoid-cycle13',header=T,sep=' ')
+flyex$time = rep(x=8460,times=length(flyex))
+flyex$distance = flyex$xcoord*5
+flyex$Bicoid = flyex$intensity*1.9
+flyex$stddev = flyex$stddev*1.9
+
+##
+with(flyex,plot(distance,Bicoid,'l',col='black',ylim=c(0,500),main='Fig.2: Diffusion rates'))
+with(flyex,lines(distance,Bicoid+stddev,'l',col='grey'))
+with(flyex,lines(distance,Bicoid-stddev,'l',col='grey'))
+legend('topright',c('literature','7.4μm²/s','4μm²/s','0.3μm²/s'),col=c('black','darkblue','dodgerblue','aquamarine'),pch=c('-','-','-','-'))
+
+high = resshape_data('./results/highdif.result')
+medium = resshape_data('./results/middif.result')
+low = resshape_data('./results/lowdif.result')
+
+with(high[high$time==8460,],lines(distance,Bicoid,'l',col='darkblue'))
+with(medium[medium$time==8460,],lines(distance,Bicoid,'l',col='dodgerblue'))
+with(low[low$time==8460,],lines(distance,Bicoid,'l',col='aquamarine'))
+##
+with(flyex,plot(distance,Bicoid,'l',col='black',ylim=c(0,500),main='Fig.3: Dynamic models at diffusion = 4μm²/s'))
+with(flyex,lines(distance,Bicoid+stddev,'l',col='grey'))
+with(flyex,lines(distance,Bicoid-stddev,'l',col='grey'))
+text=c('literature','Increasing translation rate','mRNA diffusion from fertilization','mRNA diffusion from cycle 3')
+legend('topright',text,col=c('black','red','olivedrab','forestgreen'),pch=c('-','-','-','-'))
+
+changing_trans = resshape_data('./results/Middif-c.result')
+gradient0 = resshape_data('./results/middifs.result')
+gradient3 = resshape_data('./results/middifsl.result')
+
+with(changing_trans[changing_trans$time==8460,],lines(distance,Bicoid,'l',col='red'))
+with(gradient0[gradient0$time==8460,],lines(distance,Bicoid,'l',col='olivedrab'))
+with(gradient3[gradient3$time==8460,],lines(distance,Bicoid,'l',col='forestgreen'))
+##
+with(flyex,plot(distance,Bicoid,'l',col='black',ylim=c(0,500),main='Fig.4: Complete model'))
+with(flyex,lines(distance,Bicoid+stddev,'l',col='grey'))
+with(flyex,lines(distance,Bicoid-stddev,'l',col='grey'))
+
+model = resshape_data('./results/Middifs-c.result')
+
+with(model[model$time==8460,],lines(distance,Bicoid,'p',col='red'))
+
+
+
+
+
--- analysis/resshape_data.R
+++ analysis/resshape_data.R
@@ -3,12 +3,12 @@ resshape_data <- function(file){
   pbcd_profile = read.table(file,header=FALSE)
   colnames(pbcd_profile) = c('molecule','time',seq(from=2.5,to=500,by=2.5))
   m = melt(pbcd_profile,id.vars=c("time",'molecule'))
-  res = dcast(data=m,formula = time + variable ~ molecule + .)
+  res = dcast(data=m,formula = time + variable ~ molecule + .,fun.aggregate=mean)
   colnames(res)[2]='distance'
   res[2] = as.numeric(levels(res$distance))[res$distance]
-    with(res,res$Bicoid = 1*pbcd+1*pbcd1+2*pbcd2+3*pbcd3+4*pbcd4+5*pbcd5+6*pbcd6+6*(pbcd6phb1+pbcd6phb2))
-    with(res,res$Hunchback = 1*pbcd6phb1+2*pbcd6phb2)
-    with(res,res$Caudal = pcad)
-    with(res,res$Nanos = 1*pnos+1*boundmhb+1*boundmbcd)
+    res$Bicoid = with(res,1*pbcd+1*pbcd1+2*pbcd2+3*pbcd3+4*pbcd4+5*pbcd5+6*pbcd6+6*(pbcd6phb1+pbcd6phb2))
+    res$Hunchback = with(res,1*pbcd6phb1+2*pbcd6phb2)
+    res$Caudal = with(res,pcad)
+    res$Nanos = with(res,1*pnos+1*boundmhb+1*boundmbcd)
   return(res)
 }
