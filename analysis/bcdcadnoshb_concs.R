b643f00c2d662508517aec2e31b9ffb08ecc407f
maxconway <conway.max1@gmail.com>
2013-03-11 21:12:45 +0000

    Removed correction for molecule number scaling factor in resshape_data; corrections should now be done as needed

--- analysis/bcdcadnoshb_concs.R
+++ analysis/bcdcadnoshb_concs.R
@@ -1,11 +1,11 @@
-model = resshape_data('./results/Cadnoshb-transc.result')
+model = resshape_data('./results/Cadnoshb-transc3e4t5.result')
 
-plot(0,ylim=c(0,14000),xlim=c(0,500),main='Fig.7: Bicoid, Nanos, Caudal, Hunchback',xlab='distance (μm)',ylab='molecules/μm')
+plot(0,ylim=c(0,1000000),xlim=c(0,500),main='Fig.7: Bicoid, Nanos, Caudal, Hunchback',xlab='distance (μm)',ylab='molecules/μm')
 
 text=c('Bicoid','Nanos','Caudal','Hunchback')
 legend('topright',text,col=c('blue','green','red','pink'),pch=c('-','-'))
 
-with(model[model$time==8460,],lines(distance,Bicoid,'l',col='blue'))
-with(model[model$time==8460,],lines(distance,Nanos,'l',col='green'))
-with(model[model$time==8460,],lines(distance,Caudal,'l',col='red'))
-with(model[model$time==8460,],lines(distance,Hunchback,'l',col='pink'))
+with(model[model$time==8460,],lines(distance,Bicoid*10^4,'l',col='blue'))
+with(model[model$time==8460,],lines(distance,Nanos*10^4,'l',col='green'))
+with(model[model$time==8460,],lines(distance,Caudal*10^4,'l',col='red'))
+with(model[model$time==8460,],lines(distance,Hunchback*10^4,'l',col='pink'))
