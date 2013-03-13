29d912035f744fadaf4643772efeabeeb1bc552e
maxconway <conway.max1@gmail.com>
2013-03-13 14:28:33 +0000

    a few changes to analysis, movie now made

--- analysis/bcdnos_concs.R
+++ analysis/bcdnos_concs.R
@@ -7,11 +7,11 @@ flyex$stddev = flyex$stddev/100
 ##
 png('4:bcdnos',width=640)
 
-flyex_scale = 3.0*10^5
+flyex_scale = 0.8*10^5
 
 model = resshape_data('./results/Middif-nos-e2t10.result')
 
-plot(0,ylim=c(0,2*flyex_scale),xlim=c(0,500),main='Fig.7: Bicoid, Nanos',xlab='distance (μm)',ylab='molecules (μm⁻¹)')
+plot(0,ylim=c(0,4*flyex_scale),xlim=c(0,500),main='Fig.7: Bicoid, Nanos',xlab='distance (μm)',ylab='molecules (μm⁻¹)')
 with(flyex,polygon(c(distance,rev(distance)),c((Bicoid+stddev)*flyex_scale,rev((Bicoid-stddev)*flyex_scale)),col='lightgrey',border=NA))
 
 text=c('literature','Bicoid','Nanos')
